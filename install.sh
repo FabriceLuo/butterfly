c0db725403832d18c2e00e1e5c2e38ce0941b1c0
mike <luo_minghao@163.com>
2018-08-19 22:56:50 +0800

    [ADD]add install echo

--- install.sh
+++ install.sh
@@ -12,10 +12,12 @@ USER_CUR_VIMRC="$WORK_DIR/.vimrc"
 USER_CUR_VIMRC_BAK="$WORK_DIR/.vimrc.bak"
 CUR_VIMRC="$CUR_DIR/config/vimrc"
 
-
+echo "Check old vimrc file:$USER_CUR_VIMRC"
 if [ -f  "$USER_CUR_VIMRC" ]
 then
+	echo "Backup old vimrc file:$USER_CUR_VIMRC to $USER_CUR_VIMRC_BAK"
 	mv "$USER_CUR_VIMRC"  "$USER_CUR_VIMRC_BAK"
 fi
 
+echo "Install new vimrc:$CUR_VIMRC to $USER_CUR_VIMRC"
 cp "$CUR_VIMRC" "$USER_CUR_VIMRC"
