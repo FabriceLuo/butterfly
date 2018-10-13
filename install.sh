e3cd5c5b1abcd83f371b61438e5413b42c3d5290
mike <mike@debian.debian>
2018-10-12 21:10:05 -0700

    [FIX]adjust dir structure

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ WORK_DIR="$HOME"
 CUR_DIR=`pwd`
 USER_CUR_VIMRC="$WORK_DIR/.vimrc"
 USER_CUR_VIMRC_BAK="$WORK_DIR/.vimrc.bak"
-CUR_VIMRC="$CUR_DIR/config/vimrc"
+CUR_VIMRC="$CUR_DIR/vim/vimrc"
 
 echo "Check old vimrc file:$USER_CUR_VIMRC"
 if [ -f  "$USER_CUR_VIMRC" ]
