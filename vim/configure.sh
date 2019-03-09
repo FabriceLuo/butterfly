52c6d95046ed7159ba7e43b5e4a42132ccfb5f85
luominghao <luominghao@live.com>
2019-03-09 18:54:35 +0800

    [UPDATE]update vim compile script

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-./configure --prefix=/usr/local/bin/vim --enable-perlinterp=yes --enable-pythoninterp=yes --enable-cscope --enable-terminal --enable-multibyte --enable-gui=no --enable-python3interp
+./configure --prefix=/usr/3rd/vim --enable-pythoninterp=yes --enable-cscope --enable-terminal --enable-multibyte --enable-gui=no --with-python-config-dir=
 
