e748f45bf6f4056d8ea83a217f207b71b3526c7a
luominghao <luominghao@live.com>
2019-09-07 23:03:08 +0800

    [ADD]add utils sync code

--- shell/snail/snail.sh
+++ shell/snail/snail.sh
@@ -57,11 +57,10 @@ init_environments() {
 
     env_count=$(echo "${SNAIL_CONFIG_CONTENT}" | jq ".environments | length")
 
-    # 没有环境变量需要初始化，直接返回
-    if [[ x"$env_count" == x"0" ]]; then
-        return 0
-    fi
-
+    ## 没有环境变量需要初始化，直接返回
+    #if [[ x"$env_count" == x"0" ]]; then
+    #    return 0
+    #fi
     while [[ $env_index < $env_count ]]; do
         env_type=$(echo "${SNAIL_CONFIG_CONTENT}" | jq -r ".environments[${env_index}].type" )
         env_name=$(echo "${SNAIL_CONFIG_CONTENT}" | jq -r ".environments[${env_index}].name" )
@@ -82,10 +81,59 @@ init_environments() {
 }
 
 init_utils() {
+    local util_count=
+    local util_index=0
+
+    local util_name=
+    local util_des=
+    local util_src=
+    local util_perms=
+
+    util_count=$(echo "${SNAIL_CONFIG_CONTENT}" | jq ".utils| length")
+
+    while [[ $util_index < $util_count ]]; do
+        util_des=$(echo "${SNAIL_CONFIG_CONTENT}" | jq -r ".utils[${util_index}].des" )
+        util_src=$(echo "${SNAIL_CONFIG_CONTENT}" | jq -r ".utils[${util_index}].src" )
+
+        util_name=$(echo "${SNAIL_CONFIG_CONTENT}" | jq -r ".utils[${util_index}].name" )
+        util_perms=$(echo "${SNAIL_CONFIG_CONTENT}" | jq -r ".utils[${util_index}].permissions" )
+
+        synchronize_utils "${util_name}" "${util_src}" "${util_des}" "${util_perms}"
+        if [[ $? -ne 0 ]]; then
+            echo "synchronize util(${util_name}) failed"
+        fi
+    done
+
     return 0
 }
 
 synchronize_utils() {
+    local util_name=$1
+    local util_des=$2
+    local util_src=$3
+    local util_perms=$4
+
+    local util_src_path=$(get_src_file_path "${util_src}")
+    local util_des_path=$(get_des_file_path "${util_des}")
+
+    compare_file "${util_des_path}" "${util_src_path}"
+    if [[ $? -eq 0 ]]; then
+        echo "file(${util_name}) no need to synchronize"
+        return 0
+    fi
+
+    copy_file "${util_src_path}" "${util_des_path}"
+    if [[ $? -ne 0 ]]; then
+        echo "copy util(${util_name}) from($util_src_path) to($util_des_path) failed"
+        return 1
+    fi
+
+    change_permissions "${util_des_path}"
+    if [[ $? -ne 0 ]]; then
+        echo "change util(${util_name}) file(${util_des_path}) permission(${util_perms}) failed"
+        return 1
+    fi
+
     return 0
 }
 
