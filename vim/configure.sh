0162d7594be958f4c2ed8ba63d02718788825ef9
luominghao <luominghao@live.com>
2020-05-23 19:39:32 +0800

    [ADD]add ycm extra config

--- vim/configure.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-./configure --prefix=/usr/3rd/vim --enable-pythoninterp=yes --enable-cscope --enable-terminal --enable-multibyte --enable-gui=no --with-python-config-dir=
-
---
+++ vim/plugin/YouCompleteMe/global_extra_conf.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 luominghao <luominghao@live.com>
+#
+# Distributed under terms of the MIT license.
+"""
+
+"""
+
+
+def Settings(**kwargs):
+    return {
+        'sys_path': [
+        ],
+    }
