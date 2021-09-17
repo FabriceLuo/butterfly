38d11e42eb6a62df5f4e50e692bd48e847d5070a
Fabrice Luo <fabriceluo@outlook.com>
2021-09-17 22:56:50 +0800

    Add downlaod and install makefile.

---
+++ makefile/download-install.mk
@@ -0,0 +1,32 @@
+#
+# download-install.mk
+# fabriceluo, 2021-09-17 22:18
+#
+#
+REPO            ?=
+
+PATH_USER       ?= fabrice
+PREFIX_DIR      ?= /opt
+DOWNLOAD_URL    ?=
+DOWNLOAD_ROOT   ?= ./download
+DOWNLOAD_NAME   ?= $(REPO)
+DOWNLOAD_FILE    = $(DOWNLOAD_ROOT)/$(DOWNLOAD_NAME)
+BINARY_RELATIVE ?= bin
+INSTALL_ROOT    ?= $(PREFIX_DIR)/$(REPO)
+
+.PHONY: download install
+
+download:
+	curl -L -s "$(DOWNLOAD_URL)" -o "$(DOWNLOAD_FILE)" 
+
+install:
+	mkdir -p "$(INSTALL_ROOT)"
+	tar -xf "$(DOWNLOAD_NAME)" -C "$(INSTALL_ROOT)"
+	echo "export PATH=$(INSTALL_ROOT)/${BINARY_RELATIVE}:$PATH" >> /home/$(PATH_USER)/.bashrc
+
+all: download install
+	@echo "download-install.mk needs your attention"
+
+
+# vim:ft=make
+#
